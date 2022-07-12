6910f8bceddedf9f3461e16741bd7cccc5e57b21
Sergynya174 <Sergynya174@yandex.ru>
2022-07-12 23:07:14 +0300

    15 проектная

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { cardRouter } = require('./routes/cards');
 const { login, logout, createUsers } = require('./controllers/users');
 const NotFoundError = require('./utils/errors/not-found-err');
 const Authorized = require('./middlewares/auth');
-const cors = require('./middlewares/cors');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 
@@ -21,7 +21,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors);
+app.use(cors({ credentials: true, origin: ['https://api.marina.nomoredomains.sbs', 'http://api.marina.nomoredomains.sbs', 'http://marina.nomoredomains.sbs', 'https://marina.nomoredomains.sbs', 'http://localhost:3001', 'http://localhost:3000', 'https://localhost:3001', 'https://localhost:3000', 'https://web.postman.co'] }));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
