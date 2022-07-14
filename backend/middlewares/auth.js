f7a4b2538c874ae583a62f49c04906f21d8a9cad
Sergynya174 <Sergynya174@yandex.ru>
2022-07-14 19:01:10 +0300

    15 проектная

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,16 +4,21 @@ const AuthError = require('../utils/errors/authorized-err');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const Authorized = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw (new AuthError('Необходима авторизация'));
+  } else {
+    const token = authorization.replace('Bearer ', '');
+    let payload;
 
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : '10d5865c85b895c6386a5501acfee2775244c5b6f4d2c16036b1c5858d392b34');
-  } catch (err) {
-    next(new AuthError('jwt token не валиден'));
+    try {
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : '10d5865c85b895c6386a5501acfee2775244c5b6f4d2c16036b1c5858d392b34');
+    } catch (err) {
+      next(new AuthError('jwt token не валиден'));
+    }
+    req.user = payload;
+    next();
   }
-  req.user = payload;
-  next();
 };
 
 module.exports = Authorized;
